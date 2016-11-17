2cd3431de81c734b14fc1977ae2e8f666ff47442
Conilux <assoundessens@gmail.com>
2016-11-17 22:31:31 +0100

    overdub, play stop general

--- python/channel.py
+++ python/channel.py
@@ -22,6 +22,8 @@ class channel:
 		self.isModified = True
 		#Recording
 		self.isRecording = False
+		self.isOverdubbing = False
+		self.isInvisiblePlay = True
 		self.isPlaying = False
 		self.list_of_value = []
 		self.currentIndex = 0
@@ -37,19 +39,55 @@ class channel:
 			self.isModified = True
 
 	def update( self):
-		if self.isRecording :
+		#only recording
+		if self.isRecording and not(self.isPlaying):
 			self.list_of_value.append(self.currentValue)
+			if self.isModified :
+				self.isModified = False
+				return self.currentValue
+			else :
+				return None
 
-		if self.isPlaying :
+
+		#playing and recording ( overdub )	
+		if self.isRecording and self.isPlaying:	
 			self.currentIndex+=1
+			#go to 0 if needed
 			if self.currentIndex >= len(self.list_of_value):
 				self.currentIndex = 0
-			if self.list_of_value[self.currentIndex] != self.currentValue :
+			#overdub
+			if self.isModified or self.isOverdubbing:
+				self.list_of_value[self.currentIndex]=self.currentValue
+				self.isModified = False
+				self.isOverdubbing = True
+				return self.currentValue
+			#return the value
+			elif self.list_of_value[self.currentIndex] != self.currentValue :
+				self.currentValue = self.list_of_value[self.currentIndex]
+				return self.currentValue
+			else :
+				return None	
+
+		#only playing
+		if self.isPlaying and not(self.isRecording) :
+			self.currentIndex+=1
+			if self.isModified :
+				#stop playing but let index goes on
+				self.isInvisiblePlay = True
+				self.isModified = False
+				return self.currentValue
+			if self.currentIndex >= len(self.list_of_value):
+				self.currentIndex = 0
+			if self.isInvisiblePlay :
+				return self.currentValue
+			elif self.list_of_value[self.currentIndex] != self.currentValue :
 				self.currentValue = self.list_of_value[self.currentIndex]
 				return self.currentValue
 			else :
 				return None
-		else :
+		
+		#if nothing is playing or recording 
+		if not(self.isPlaying) and not(self.isRecording) :
 			if self.isModified :
 				self.isModified = False
 				return self.currentValue
@@ -58,22 +96,25 @@ class channel:
 
 			
 	def playStop( self, isPlay ):
-		if self.isPlay and len(self.list_of_value)>0 :
+		if isPlay and len(self.list_of_value)>0 :
 			self.currentIndex = 0
 			self.isPlaying = True
-			print self.name+" play "
+			self.isInvisiblePlay = False
+			#print self.name+" play "
 		else :
 			self.isPlaying = False
-			print self.name+" stop"
+			#print self.name+" stop"
 
 	def startRecording( self):
-		del self.list_of_value[:]
-		self.isPlaying = False
+		#if not playing, delete the previous recording
+		if not(self.isPlaying):
+			del self.list_of_value[:]
 		self.isRecording = True
+		self.isOverdubbing = False
 
 	def stopRecording (self):
 		self.isRecording = False
-		self.isPlaying =True
+		self.isOverdubbing = False
 
 
 
