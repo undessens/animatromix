90ac3de9ec309427f69703ee3b7baf5e4e9e3559
Un Des Sens <assoundessens@gmail.com>
2018-10-01 17:38:41 +0000

    anima v2

--- of/src/main.cpp
+++ of/src/main.cpp
@@ -16,24 +16,24 @@ int main()
 #if (OF_VERSION_MINOR == 9)
     ofGLESWindowSettings settings;
     settings.width = 1280;
-    settings.height = 720;
+    settings.height = 768;
     settings.setGLESVersion(2);
     ofCreateWindow(settings);
 #else
     ofSetLogLevel("ofThread", OF_LOG_ERROR);
     ofSetCurrentRenderer(ofGLProgrammableRenderer::TYPE);
-    ofSetupOpenGL(1280, 720, OF_FULLSCREEN);
+    ofSetupOpenGL(1280, 768, OF_FULLSCREEN);
 #endif
 #else
 #ifdef FORCE_PROGRAMMMABLE
     ofGLWindowSettings glWindowSettings;
     glWindowSettings.width = 1280;
-    glWindowSettings.height = 720;
+    glWindowSettings.height = 768;
     glWindowSettings.setGLVersion(3, 2);
     ofCreateWindow(glWindowSettings);
 #else
     ofSetLogLevel("ofThread", OF_LOG_ERROR);
-    ofSetupOpenGL(1280, 720, OF_FULLSCREEN);
+    ofSetupOpenGL(1280, 768, OF_FULLSCREEN);
 );
 #endif
     
